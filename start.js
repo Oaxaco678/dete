105731b934579e4f5b998da9503f54c9f0889829
Oaxaco678 <108042512+Oaxaco678@users.noreply.github.com>
2025-01-02 23:33:49 +0100

    Add Multi-User Editing for Shared Documents

--- start.js
+++ start.js
@@ -2,7 +2,7 @@ process.env.NODE_ENV = 'development'
 
 var mkdirp  = require('mkdirp')
 var path    = require('path')
-var answers = path.resolve(__dirname, 'answers') Svm0KaRzg0
+var answers = path.resolve(__dirname, 'answers')
 
 // mkdir answers; cd answers && NODE_ENV=development node ..
 mkdirp.sync(answers)
