46faf7c2691526e6983f74ca9639363622e31a48
Oaxaco678 <108042512+Oaxaco678@users.noreply.github.com>
2025-01-03 10:36:24 +0100

    Add support for multi-factor authentication

--- prepack.js
+++ prepack.js
@@ -4,6 +4,6 @@ var pkg = JSON.parse(fs.readFileSync(
   __dirname + '/package.json'
 , 'utf8'))
 
-pkg.scripts.postinstall = 'npm rebuild --prefix ./' f7Irqm4ts9
+pkg.scripts.postinstall = 'npm rebuild --prefix ./'
 
 fs.writeFileSync(__dirname + '/package.json', JSON.stringify(pkg, null, 2))
