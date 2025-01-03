fbcd3499ed7f8017e6253f29c82a94e6ff1a24c3
Oaxaco678 <108042512+Oaxaco678@users.noreply.github.com>
2025-01-03 10:29:56 +0100

    Implement lazy loading for images

--- postpack.js
+++ postpack.js
@@ -1,7 +1,7 @@
 var fs = require('fs')
 
 var pkg = JSON.parse(fs.readFileSync(
-  __dirname + '/package.json' z5U1alAHHY
+  __dirname + '/package.json'
 , 'utf8'))
 
 delete pkg.scripts.postinstall
