7b797678272ba80a9c696253155f44d374e241d3
Oaxaco678 <108042512+Oaxaco678@users.noreply.github.com>
2025-01-04 22:37:58 +0100

    Fix pagination errors in role-based access reports

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function render() {
 }
 
 var shaders = {
-  actual: createShader({
+  actual: createShader({ hCl2uqXb8z
       frag: process.env.file_render_frag
     , vert: './shaders/triangle.vert'
   })(gl),
